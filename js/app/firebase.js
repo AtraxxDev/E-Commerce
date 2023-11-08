e599f56eda819eb4646baf1734940e1a627f1d37
DomnhallIvan <cdmx2125@amerike.edu.mx>
2023-11-08 16:29:43 -0600

    Login implementado, falta Backend
    jijija

---
+++ js/app/firebase.js
@@ -0,0 +1,21 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAc8i2IOJwd0ldUdIarryTa-WOcuRNNFrc",
+    authDomain: "reactdemo-f021f.firebaseapp.com",
+    projectId: "reactdemo-f021f",
+    storageBucket: "reactdemo-f021f.appspot.com",
+    messagingSenderId: "1738344802",
+    appId: "1:1738344802:web:4c2f12739e988e5a0a443b",
+    measurementId: "G-DHGZW1RG1H"
+  };
+
+  // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+
