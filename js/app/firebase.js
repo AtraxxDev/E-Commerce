4aa71914c28fdb8a8d1d14657f95887f94695624
LDom890 <montecinonestor890@gmail.com>
2023-11-13 18:18:04 -0600

    Almacenamiento de usuarios, contraseñas, y errores en el registro base

--- js/app/firebase.js
+++ js/app/firebase.js
@@ -1,6 +1,7 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,5 @@
 
   // Initialize Firebase
  export const app = initializeApp(firebaseConfig);
+ export const getAuth(app)
 
--- js/app/signupform.js
+++ js/app/signupform.js
@@ -1,8 +1,40 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
+import {getAuth} from './firebase.js'
 const signupForm=document.querySelector('signup-form')
 
-signupForm.addEventListener('submit',(e)=>
+signupForm.addEventListener('submit',async (e)=>
 {
     e.preventDefault();
     const email=signupForm['signup-email'].value
     const password=signupForm['signup-password'].value
+
+    try{
+        const userCredentials=createUserWithEmailAndPassword(getAuth,email,password)
+        console.log(userCredentials)
+
+        //cierra el modal de bootstrap
+        const signupModal=document.querySelector('#signupModal')
+        const modal= bootstrap.Modal.getInstance(signupModal)
+        modal.hide()
+    }catch(error)
+    {
+        console.log(error.message)
+        console.log(error.code)
+     
+        if(error.code==='auth/email-already-in-use')
+        {
+            alert('Email already in use')
+        }     
+        if(error.code==='auth/invalid-email')
+        {
+            alert('Invalid email')
+        }else if (error.code==='auth/weak-password')
+        {
+            alert('Password is too weak')
+        }else if(error.code){
+            alert('NANI?????')
+        }
+    }
+    
+
 })
