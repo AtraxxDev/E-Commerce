1cc101eeac70674acbbb73886cb868d9745e434f
Hector Otero <cdmx2292@amerike.edu.mx>
2023-11-15 17:08:36 -0600

    Base API

--- js/SearchBar.js
+++ js/SearchBar.js
@@ -1,3 +1,33 @@
+async function obtenerDatosLocales() {
+  let response = await fetch("json/API.json");
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
+
+
+  let $subject = document.querySelector("#info-shida");
+  let Materias = "";
+
+  data.Materias.forEach(function (el) {
+    Materias += `
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card" style="width: 36rem;">          
+          <div class="card-body">
+            <h2 class="card-title">${el.Producto}</h2>
+            <p class="card-text">${el.Precio}</p>
+            <p class="card-text">${el.Imagen}</p>
+            <p class="card-text">${el.Descripción}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  $subject.innerHTML = Materias;
+}
+
+obtenerDatosLocales();
+
 firebase.initializeApp(config);
 var firestore = firebase.firestore();
 var docRef = firestore.collection("indian");
