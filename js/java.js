ec5cfc3c272f2dddf2476b95eef2fdf0c401d714
Elliot Reyes <gametraxworks@gmail.com>
2023-11-23 11:46:10 -0600

    Agregando pagina productos
    - Se agrego la pagina de productos

--- js/java.js
+++ js/java.js
@@ -11,3 +11,25 @@ menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const quantityElement = document.getElementById('quantity');
+    const incrementButton = document.getElementById('increment');
+    const decrementButton = document.getElementById('decrement');
+
+    // Increment quantity
+    incrementButton.addEventListener('click', function () {
+        let currentQuantity = parseInt(quantityElement.textContent);
+        currentQuantity++;
+        quantityElement.textContent = currentQuantity;
+    });
+
+    // Decrement quantity, but not below 1
+    decrementButton.addEventListener('click', function () {
+        let currentQuantity = parseInt(quantityElement.textContent);
+        if (currentQuantity > 1) {
+            currentQuantity--;
+            quantityElement.textContent = currentQuantity;
+        }
+    });
+});
